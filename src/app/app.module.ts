8ecbb7d853de4a853008c1c41d3d46742c750c0d
cmargok <alphackode@gmail.com>
2024-09-09 14:58:14 -0500

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FatherComponent } from './father/father.component';
-import { SonComponent } from './son/son.component';
 import { FormsModule } from '@angular/forms';
-import { HermanoComponent } from './hermano/hermano.component';
-import { SiblingsStyleDirective } from './siblings-style.directive';
-import { MyCustomPipePipe } from './my-custom-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FatherComponent,
-    SonComponent,
-    HermanoComponent,
-    SiblingsStyleDirective,
-    MyCustomPipePipe
   ],
   imports: [
     BrowserModule,
