e46d2b5b7146f411d7fa7948b5947967824798bd
cmargok <alphackode@gmail.com>
2024-09-14 15:33:19 -0500

    implemtnation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
